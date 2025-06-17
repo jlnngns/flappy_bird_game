2e0ec6c9a9f78389afdc86446bb8488f166339bc
Julianna Gines <jmgines.12@gmail.com>
2025-06-17 21:55:26 +0800

    Create guidelines for ui.py

---
+++ ui.py
@@ -0,0 +1,11 @@
+import pygame
+
+class UIManager:
+    def __init__(self, font):
+
+    def draw_score(self, screen, score):
+
+    def draw_game_over(self, screen, score, high_score):
+
+    def draw_menu(self, screen):
+        
