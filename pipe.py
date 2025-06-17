f7c91cf0766ef88d15e425a20535e97a0f7b1305
Julianna Gines <jmgines.12@gmail.com>
2025-06-17 21:47:35 +0800

    Created guidelines for pipe.py

---
+++ pipe.py
@@ -0,0 +1,16 @@
+import pygame
+import random
+
+class PipeManager:
+    def __init__(self):
+
+    def spawn_pipe(self):
+
+    def move_pipes(self):
+
+    def draw_pipes(self, screen):
+
+    def check_collision(self):
+
+    def reset(self):
+        
