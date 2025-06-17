bcefce937f3b601cc09e2f74d54c688cfcac19c0
Julianna Gines <jmgines.12@gmail.com>
2025-06-17 21:38:55 +0800

    Created guidlines for bird.py

---
+++ bird.py
@@ -0,0 +1,17 @@
+import pygame
+
+class Bird:
+    def __init__(self):
+
+    def flap(self):
+
+    def animate(self):
+
+    def rotate(self):
+
+    def update(self, gravity):
+
+    def draw(self, screen):
+
+    def reset(self):
+        
