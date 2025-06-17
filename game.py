054782c5b4edf239c01b3b2311187b2bf1d6d730
Julianna Gines <jmgines.12@gmail.com>
2025-06-17 21:30:17 +0800

    Create guidlines for game.py

---
+++ game.py
@@ -0,0 +1,24 @@
+import pygame, sys
+from bird import Bird
+from pipe import PipeManager
+from ui import UIManager
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        
+    def setup_events(self):
+
+    def load_sounds(self):
+
+    def reset_game(self):
+
+    def run(self):
+
+    def handle_events(self):
+
+    def update(self):
+
+    def check_score(self):
+
+    def draw(self):
