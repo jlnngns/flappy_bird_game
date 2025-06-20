a022e8f461e373cf2dcd37a227484fece4c27479
Julianna Gines <jmgines.12@gmail.com>
2025-06-20 21:53:34 +0800

    Create Bird class to encapsulate bird behavior

--- bird.py
+++ bird.py
@@ -2,16 +2,30 @@ import pygame
 
 class Bird:
     def __init__(self):
+        self.frames = [pygame.transform.scale2x(pygame.image.load('assets/bluebird-downflap.png').convert_alpha()), pygame.transform.scale2x(pygame.image.load('assets/bluebird-midflap.png').convert_alpha()), pygame.transform.scale2x(pygame.image.load('assets/bluebird-upflap.png').convert_alpha())]
+        self.index = 0
+        self.image = self.frames[self.index]
+        self.rect = self.image.get_rect(center=(100, 512))
+        self.movement = 0
 
     def flap(self):
+        self.movement = 0
+        self.movement -= 12
 
     def animate(self):
+        self.index = (self.index + 1) % len(self.frames)
+        self.image = self.frames[self.index]
 
     def rotate(self):
+        return pygame.transform.rotozoom(self.image, -self.movement * 3, 1)
 
     def update(self, gravity):
+        self.movement += gravity
+        self.rect.centery += self.movement
 
     def draw(self, screen):
+        screen.blit(self.rotate(), self.rect)
 
     def reset(self):
-        
+        self.rect.center = (100, 512)
+        self.movement = 0
